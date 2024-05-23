d993c5757ed35e5899e41d1476e0e5c4d5395b8c
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-23 19:18:07 +0800

    s

--- src/System.cpp
+++ src/System.cpp
@@ -4,19 +4,22 @@
 #include <string>
 #include <array>
 
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
+namespace System {
+    std::string exec(const char* cmd) {
+        std::array<char, 128> buffer;
+        std::string result;
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+        if (!pipe) {
+            throw std::runtime_error("popen() failed!");
+        }
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            result += buffer.data();
+        }
+        return result;
     }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
 }
 
+/*
 int main() {
     std::string command = "add -a 10 -b 20";
     try {
@@ -27,3 +30,4 @@ int main() {
     }
     return 0;
 }
+*/
