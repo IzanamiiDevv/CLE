916c762a0db99bd82087d58f19604ebce6c7cf0e
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-24 00:20:03 +0800

    s

--- src/System.cpp
+++ src/System.cpp
@@ -20,17 +20,3 @@ namespace System {
         }
         return result;
     }
-}
-
-/*
-int main() {
-    std::string command = "add -a 10 -b 20";
-    try {
-        std::string output = exec(command.c_str());
-        std::cout << "Command output: " << output << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-    return 0;
-}
-*/
